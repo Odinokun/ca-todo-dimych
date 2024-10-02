67bba27730a5214ef2f497500776d8529231dba3
odinokun <odinokun@gmail.com>
2024-10-02 12:22:50 -0400

    Add Redux task to App and refactor Todolist to render tasks dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
     { id: 1, title: 'HTML&CSS', isDone: true },
     { id: 2, title: 'JS', isDone: true },
     { id: 3, title: 'React', isDone: false },
+    { id: 4, title: 'Redux', isDone: false },
   ];
 
   const tasks2: ITask[] = [
